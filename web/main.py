8062d1c318a307c2c6f0cf0a2ccae1b75583e5ad
Afolabi Abass <afolabi.abass@hotmail.co.uk>
2020-01-07 22:57:08 +0100

    - configure route to take data from request

--- web/main.py
+++ web/main.py
@@ -5,6 +5,9 @@ import uuid
 import redis
 from fastapi import FastAPI, HTTPException
 from starlette.requests import Request
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
 db = redis.StrictRedis(host=os.environ.get("REDIS_HOST"))
@@ -17,42 +20,43 @@ def index():
     return "Hello World!"
 
 
-@app.post("/predict")
-def predict(request: Request):
+@app.get("/predict")
+def predict(request: Request, count: int = 0, more: int = 0, keyword: int = 0, rate: int = 0):
     data = {"success": False}
 
-    if request.method == "POST":
-        # Generate an ID for the classification then add the classification ID + image to the queue
-        k = str(uuid.uuid4())
-        # replace input with actual input from the api request
-        d = {"id": k, "input": [[5, 116, 74, 0, 0, 25.6, 0.201, 30]]}
-        db.rpush('query_queue', json.dumps(d))
+    if count == 0 and more == 0 and keyword == 0 and rate == 0:
+        return data
 
-        # Keep looping for CLIENT_MAX_TRIES times
-        num_tries = 0
-        while num_tries < CLIENT_MAX_TRIES:
-            num_tries += 1
+    # Generate an ID for the classification then add the classification ID + request
+    k = str(uuid.uuid4())
+    # replace input with actual input from the api request
+    d = {"id": k, "input": [[count, more, keyword, rate]]}
+    db.rpush(os.environ.get("QUERY_QUEUE"), json.dumps(d))
 
-            # Attempt to grab the output predictions
-            output = db.get(k)
+    # Keep looping for CLIENT_MAX_TRIES times
+    num_tries = 0
+    while num_tries < CLIENT_MAX_TRIES:
+        num_tries += 1
 
-            # Check to see if our model has classified the input image
-            if output is not None:
-                # Add the output predictions to our data dictionary so we can return it to the client
-                output = output.decode("utf-8")
-                data["predictions"] = json.loads(output)
+        # Attempt to grab the output predictions
+        output = db.get(k)
 
-                # Delete the result from the database and break from the polling loop
-                db.delete(k)
-                break
+        # Check to see if our model has classified the input image
+        if output is not None:
+            # Add the output predictions to our data dictionary so we can return it to the client
+            output = output.decode("utf-8")
+            data["predictions"] = json.loads(output)
 
-            # Sleep for a small amount to give the model a chance to classify the input image
-            time.sleep(float(os.environ.get('CLIENT_SLEEP')))
+            # Delete the result from the database and break from the polling loop
+            db.delete(k)
+            break
 
-            # Indicate that the request was a success
-            data["success"] = True
-        else:
-            raise HTTPException(status_code=400, detail="Request failed after {} tries".format(CLIENT_MAX_TRIES))
+        # Sleep for a small amount to give the model a chance to classify the input image
+        time.sleep(float(os.environ.get('CLIENT_SLEEP')))
+
+        # Indicate that the request was a success
+        data["success"] = True
+
 
     # Return the data dictionary as a JSON response
     return data
