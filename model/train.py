0a5f56eedf1175681d4beaffecb5703703bae4f9
Afolabi Abass <afolabi.abass@hotmail.co.uk>
2020-01-07 23:25:55 +0100

    wrap training in try block

--- model/train.py
+++ model/train.py
@@ -54,22 +54,25 @@ def loadFromDb(path='test-training-data-2.csv'):
 
 
 def train():
-    # define the keras model
-    loadFromDb()
-    model = Sequential()
-    model.add(Dense(12, input_dim=4, activation='relu'))
-    model.add(Dense(8, activation='relu'))
-    model.add(Dense(1, activation='sigmoid'))
-    # compile the keras model
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    # fit the keras model on the dataset
-    model.fit(X, y, epochs=150, batch_size=10, steps_per_epoch=None)
+    try:
+        # define the keras model
+        loadFromDb()
+        model = Sequential()
+        model.add(Dense(12, input_dim=4, activation='relu'))
+        model.add(Dense(8, activation='relu'))
+        model.add(Dense(1, activation='sigmoid'))
+        # compile the keras model
+        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+        # fit the keras model on the dataset
+        model.fit(X, y, epochs=150, batch_size=10, steps_per_epoch=None)
 
-    # evaluate the keras model
-    _, accuracy = model.evaluate(X, y)
-    print('Accuracy: %.2f' % (accuracy * 100))
+        # evaluate the keras model
+        _, accuracy = model.evaluate(X, y)
+        print('Accuracy: %.2f' % (accuracy * 100))
 
-    save(model)
+        save(model)
+    except:
+        print('Error occurred while trying to train data')
 
 
 def save(model):
